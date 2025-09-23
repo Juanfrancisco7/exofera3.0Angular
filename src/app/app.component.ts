50d785dd9f0a4e01a37d055e4ca7a4a766c4103b
Juanfrancisco7 <juanbencomo25@gmail.com>
2025-09-23 15:13:33 +0200

    feat: termina la migración del header con estilos responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+// Paso 1: Importa tu HeaderComponent aquí
+import { HeaderComponent } from './components/header/header.component'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  // Paso 2: Añade HeaderComponent a la lista de imports
+  imports: [
+    RouterOutlet, 
+    HeaderComponent 
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
